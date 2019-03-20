a961b95892cf6942ba55df912c924c9da600dd48
Ruplekha Singh <ruplekha.singh@bellevuecollege.edu>
2019-03-19 20:24:43 -0700

    Add Post Test

--- RestApiAutomation/RestApiAutomation/ApiTests.cs
+++ RestApiAutomation/RestApiAutomation/ApiTests.cs
@@ -15,11 +15,43 @@ namespace RestApiAutomation
             string url = String.Format("{0}todos/1", _baseUrl);
             RestClient client = new RestClient();
             RestRequest request = new RestRequest(url, Method.GET);
-            request.AddHeader("Accept", "application/json");
+            //request.AddHeader("Accept", "application/json");
 
             Data queryResult = client.Execute<Data>(request).Data;
+            //Assert.GetUrl();
 
-            Console.WriteLine(queryResult.ToString());
+            //Console.WriteLine(queryResult.ToString());
+        }
+        [Test]
+        public void PostResource()
+        {
+            Data toPost = new Data();
+            toPost.id = 43;
+            toPost.userId = 34;
+            toPost.title = "test";
+            toPost.completed = true;
+            
+            string url = String.Format("{0}posts", _baseUrl);
+            RestClient client = new RestClient();
+            RestRequest request = new RestRequest(url, Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            
+            request.AddHeader("Accept", "application/json");
+            request.AddBody(toPost);
+
+            Data queryResult = client.Execute<Data>(request).Data;
+            Assert.AreEqual(101, queryResult.id);
+                
+            //request.Addbody(new
+            //{
+            //    id = 43,
+            //    userId = 34,
+            //    title "Test"
+
+
+            //});
+            //Console.WriteLine(queryResult.ToString());
+        
         }
     }
 }
